008b5f53d3ba6d535e951cb349d0f3a3d61ceccf
ADHINAJAYAL <adhinajay712@gmail.com>
2025-05-20 16:39:48 +0530

    first commit

---
+++ WEEK3/MAY20/agents/debugger.py
@@ -0,0 +1,13 @@
+from openai import AsyncOpenAI
+from typing import Any
+
+class Debugger:
+    def __init__(self):
+        self.client = AsyncOpenAI(base_url="https://generativelanguage.googleapis.com/v1beta", api_key="AIzaSyCMK2D0I_qW4gVrlQd45tQ6cv1OOR7Mp90")
+
+    async def respond(self, prompt: str) -> str:
+        response = await self.client.chat.completions.create(
+            model="models/gemini-1.5-flash",
+            messages=[{"role": "user", "content": prompt}],
+        )
+        return response.choices[0].message.content
