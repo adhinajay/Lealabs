6c7b4736bd93197cfcb631d1ca9d2c1885e380a6
ADHINAJAYAL <adhinajay712@gmail.com>
2025-05-20 16:40:50 +0530

    first commit

--- WEEK3/MAY20/agents/coder.py
+++ WEEK3/MAY20/agents/coder.py
@@ -3,7 +3,7 @@ from typing import Any
 
 class Coder:
     def __init__(self):
-        self.client = AsyncOpenAI(base_url="https://generativelanguage.googleapis.com/v1beta", api_key="AIzaSyCMK2D0I_qW4gVrlQd45tQ6cv1OOR7Mp90")
+        self.client = AsyncOpenAI(base_url="https://generativelanguage.googleapis.com/v1beta", api_key="######")
 
     async def respond(self, prompt: str) -> str:
         response = await self.client.chat.completions.create(
